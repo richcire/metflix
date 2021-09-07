66847d944f7ece3be959e5ae0ff2d2c73373ceb6
minseokyeon <eric9907@naver.com>
2021-09-08 00:05:12 +0900

    added inner scrollview, bottom navigator

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -15,16 +15,49 @@ class MainScreen extends StatelessWidget {
           SliverList(
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
-                return Container(
-                  color: Colors.blueGrey,
-                  margin: EdgeInsets.symmetric(vertical: 10),
-                  height: 100,
-                );
+                if (index == 0) {
+                  return Container(
+                    color: Colors.blueGrey,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    height: 500,
+                  );
+                  ;
+                } else {
+                  return Container(
+                    color: Colors.blueGrey,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                    height: 150,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 7,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: EdgeInsets.all(10),
+                          color: Colors.blue,
+                          height: 80,
+                          width: 50,
+                        );
+                      },
+                    ),
+                  );
+                }
               },
-              childCount: 20,
+              childCount: 10,
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Container(
+            //Rap Icon in Container
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Icon(Icons.home), Icon(Icons.video_library_outlined), Icon(Icons.arrow_circle_down_sharp)],
+            ),
+          ),
+        ),
       ),
     );
   }
